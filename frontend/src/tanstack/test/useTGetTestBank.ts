e1816b13fb5259d92d32cabef9e15ed9551ab7bd
envien <novrandobilly@gmail.com>
2026-08-01 14:59:42 +0700

    feat: add TEST_ORDER constant and implement sorting for test bank items based on slug

--- frontend/src/tanstack/test/useTGetTestBank.ts
+++ frontend/src/tanstack/test/useTGetTestBank.ts
@@ -16,6 +16,22 @@ export interface TestBankItem {
   updated: string;
 }
 
+const TEST_ORDER = [
+  "/papikostick",
+  "/disc",
+  "/eas4",
+  "/eas5",
+  "/eas6",
+  "/eas7",
+  "/eas10",
+  "/a5",
+  "/dr",
+  "/da5",
+  "/st17",
+  "/intray1",
+  "/intray2",
+];
+
 export const useTGetTestBank = () => {
   const queryResponse = useQuery({
     queryKey: ["test-bank"],
@@ -23,6 +39,13 @@ export const useTGetTestBank = () => {
       const response: TestBankItem[] = await pb
         .collection("test_bank")
         .getFullList();
+
+      response.sort((a, b) => {
+        const indexA = TEST_ORDER.indexOf(a.slug);
+        const indexB = TEST_ORDER.indexOf(b.slug);
+        return (indexA === -1 ? 999 : indexA) - (indexB === -1 ? 999 : indexB);
+      });
+
       return response;
     },
   });
